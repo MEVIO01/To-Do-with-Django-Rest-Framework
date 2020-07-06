09c49be3e547ed5fdf8ecda4bcc8d115cc6f0ebe
MEVIO01 <erik.mirai@outlook.com>
2020-07-06 14:01:10 +0300

    frontend created. GetCookie, buildList, addEventListener added

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -20,7 +20,7 @@ def api_overview(request):
 
 @api_view(['GET'])
 def tasks_view(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by("-id")
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
 
